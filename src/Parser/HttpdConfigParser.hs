15930c71f48b13cac529b94c779a54af9b2e0e2d
Stephen O'Brien <wayofthepie@gmail.com>
2014-07-17 21:25:24 +0100

    Cleanup parsers and data types.
    Removes a lot of types that were pretty much just boilerplate.

--- src/Parser/HttpdConfigParser.hs
+++ src/Parser/HttpdConfigParser.hs
@@ -23,9 +23,7 @@
     This is a (massive) work in progress.
 -}
 
-module Parser.HttpdConfigParser(            
-            Config,
-            configp) where
+module Parser.HttpdConfigParser where
 
 import Text.Parsec hiding (Line)
 import Text.Parsec.String
@@ -33,119 +31,26 @@ import Control.Applicative hiding ((<|>), many)
 import Control.Monad
 
 -------------------------------------------------------------------------------
--- Classes
--------------------------------------------------------------------------------
-class DirectiveS a where 
-    directiveName       :: a -> String
-    directiveArgs       :: a -> [String]
-    hasNestedConfig     :: a -> Bool
-    nestedConfig        :: a -> Config
-
--------------------------------------------------------------------------------
 -- Config
 -------------------------------------------------------------------------------
-newtype Config = Config [DirectiveT] deriving (Eq, Show)
+newtype Config = Config [AbsDirective] deriving (Eq, Show)
 
-extractDirectives :: Config -> [DirectiveT]
+emptyConfig :: Config
+emptyConfig = Config ( [] )
+
+extractDirectives :: Config -> [AbsDirective]
 extractDirectives ( Config([]) )    = []
 extractDirectives ( Config(x) )     = x
 
-emptyConfig :: Config 
-emptyConfig = Config([])  
+data AbsDirective   = D Directive | S SDirective deriving (Eq, Show)
 
-getModules :: Config -> [[String]]
-getModules (Config(x:xs))  = if isModuleLoad x then
-                                getModules' (directiveArgs x :[]) xs
-                              else
-                                getModules' [] xs
+data Directive      = Directive String [String] deriving (Eq, Show)
 
-getModules' :: DirectiveS a => [[String]] -> [a] -> [[String]]
-getModules' a [] = a
-getModules' a (x : xs) 
-    | isModuleLoad x =  getModules' (directiveArgs x : a) xs
-    | otherwise = getModules' a xs
-                          
-isModuleLoad :: DirectiveS a => a -> Bool
-isModuleLoad x = directiveName x == "LoadModule"                          
-                          
-getConfig :: Either ParseError Config -> Config
-getConfig x = case x of 
-   Left err -> emptyConfig
-   Right c  -> c
+data SDirective     = SDirective SOpen Config SClose deriving (Eq, Show)
+                  
+data SOpen          = SOpen (Directive) deriving (Eq, Show)
 
---mapConfig :: (DirectiveS -> b) -> Config -> [b]
---mapConfig f (Config xs) = map f xs
-
--------------------------------------------------------------------------------
--- Line
--------------------------------------------------------------------------------
-data DirectiveT  = Section SectionDirective 
-                    | Simple SimpleDirective deriving (Eq, Show)
-
-instance DirectiveS DirectiveT where
-    directiveName ( Section x )     = directiveName x
-    directiveName ( Simple x)       = directiveName x
-
-    directiveArgs ( Simple x )      = directiveArgs x
-    directiveArgs ( Section x )     = directiveArgs x
-
-    hasNestedConfig ( Section x )   = hasNestedConfig x
-    hasNestedConfig ( Simple x)     = hasNestedConfig x
-
-    nestedConfig (Section x)        = nestedConfig x
-    nestedConfig (Simple x)         = nestedConfig x
-                   
--------------------------------------------------------------------------------
--- SectionDirective
--------------------------------------------------------------------------------
-data SectionDirective   
-        = SectionDirective SectionOpen Config SectionClose deriving (Eq, Show)
-
-{-
-    hasCorrectTags : whether given section directive's open/close tags match.
--}
-hasCorrectTags :: SectionDirective -> Bool
-hasCorrectTags ( SectionDirective ( SectionOpen sd ) _ ( SectionClose sc ) ) 
-        | directiveName sd == sc    = True
-        | otherwise                 = False
-
-
-instance DirectiveS SectionDirective where
-    
-    directiveName 
-        (SectionDirective (SectionOpen (SimpleDirective x _)) _ _)  = x
-    
-    directiveArgs 
-        (SectionDirective (SectionOpen (SimpleDirective _ x)) _ _)  = x
-    
-    hasNestedConfig _                                               = True
-    
-    nestedConfig (SectionDirective _ x _ )                          = x
-
-   
--------------------------------------------------------------------------------
--- SimpleDirective
--------------------------------------------------------------------------------
-data SimpleDirective    = SimpleDirective String [String] deriving (Eq, Show)
-
-instance DirectiveS SimpleDirective where
-    directiveName ( SimpleDirective ( x ) _)    = x
-    directiveArgs ( SimpleDirective _ ( x ) )   = x
-    hasNestedConfig _                           = False
-    nestedConfig _                              = emptyConfig
-
-    
-
--------------------------------------------------------------------------------
--- SectionOpen
--------------------------------------------------------------------------------
-data SectionOpen    = SectionOpen SimpleDirective deriving (Eq, Show)
-
-
--------------------------------------------------------------------------------
--- SectionClose
--------------------------------------------------------------------------------
-data SectionClose   = SectionClose String deriving (Eq, Show)
+data SClose         = SClose String deriving (Eq, Show)
 
 
 
@@ -160,22 +65,22 @@ data SectionClose   = SectionClose String deriving (Eq, Show)
 -}
 configp :: Parser Config
 configp = Config <$> many1 directivep
-    
+
     
 {-
     directivep : parse a directive
 -}
-directivep :: Parser DirectiveT
+directivep :: Parser AbsDirective
 directivep = skipMany whitespace
     *> skipMany commentp 
-    *> ( try ( Section <$> sectionDirectivep )
-        <|> ( Simple <$> simpleDirectivep ) <?> "Directive" ) 
+    *> ( try ( S <$> sectionDirectivep )
+        <|> ( D <$> simpleDirectivep ) <?> "Directive" ) 
     <* skipMany commentp 
     <* skipMany whitespace
     
     
-sectionDirectivep :: Parser SectionDirective
-sectionDirectivep = SectionDirective
+sectionDirectivep :: Parser SDirective
+sectionDirectivep = SDirective
     <$> sectionOpenp   
     <*> ( try ( lookAhead ( sectionClosep >> return emptyConfig ) )
                     <|> configp <?> "SectionClose or Config" )
@@ -190,22 +95,22 @@ sectionDirectivep = SectionDirective
     by one or more spaces and the arguments, if more than one, must 
     also be seperated from each other by one or more spaces
 -}
-simpleDirectivep :: Parser SimpleDirective
-simpleDirectivep = SimpleDirective 
-    <$> directiveNamep 
+
+simpleDirectivep :: Parser Directive
+simpleDirectivep = fmap Directive 
+     directiveNamep 
     <*  ( many $ oneOf " " ) 
     <*> ( endBy directiveArgp $ many $ oneOf " " )
    
- 
     
-sectionOpenp :: Parser SectionOpen
-sectionOpenp = SectionOpen 
+sectionOpenp :: Parser SOpen
+sectionOpenp = SOpen 
     <$> ( char '<' *> simpleDirectivep <* char '>' <*  skipMany newline ) 
     <?> "SectionOpen"    
 
     
-sectionClosep :: Parser SectionClose
-sectionClosep = SectionClose
+sectionClosep :: Parser SClose
+sectionClosep = SClose
     <$> ( char '<' *> char '/' *> directiveNamep <* char '>' )  
     <* skipMany whitespace 
     <?> "SectionClose"
@@ -238,21 +143,6 @@ commentp = char '#'
     *> try ( skipMany ( noneOf "\n\r" ) ) 
     *> skipMany whitespace
        
-    
-{-
-    Parse an integer.
--}
-num :: Parser Int
-num = read <$> many digit
-
-
-{-
-    Parse a string literal.
--}
-lit :: String -> Parser String
-lit xs = string xs <* whitespace
-
-
 {-
     Parse whitespace - includes newlines.
 -}
