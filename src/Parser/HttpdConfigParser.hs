e91b5debeadae77e81fd81f6b5b5ef3768ccbc4a
Stephen O'Brien <wayofthepie@gmail.com>
2014-07-23 21:04:42 +0100

    Remove module comment.

--- src/Parser/HttpdConfigParser.hs
+++ src/Parser/HttpdConfigParser.hs
@@ -1,27 +1,3 @@
-{-
-    A parser for Apache 2.2 configuration files.
-    
-    Following is what the grammar for an apache httpd.conf
-    (and other config files) should be (this is taken from an old book on 
-    configuring apache 2.0):
-    
-    config              ::= directive*
-    directive           ::= section-directive | simple-directive
-    section-directive   ::= section-open configuration section-closed
-    section-open        ::= "<" directive-name directive-argument* ">"
-    section-close       ::= "</" directive-name ">"
-    simple-directive    ::= directive-name directive-argument*
-    
-    directive-name      ::= ...
-        (see http://httpd.apache.org/docs/2.2/mod/directives.html for a list
-            of possible directives and their arguments)
-            
-    directive-arg       ::= ...
-        (see http://httpd.apache.org/docs/2.2/mod/directives.html for a list
-            of possible directives and their arguments)
-            
-    This is a (massive) work in progress.
--}
 
 module Parser.HttpdConfigParser where
 
