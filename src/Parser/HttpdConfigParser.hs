ef05bb996824a2bacd768f547bcd6fc6c093d9ee
Stephen O'Brien <wayofthepie@gmail.com>
2014-08-02 15:32:22 +0100

    Consolidate to single type.
    Removes boilerplate data types. Parsing now produces Config [Directive].
    Directives are redefined as:
    
    Directive name args [Directive]
    
    This keeps the logical semantic structure of Section Directives but
    allows for a less complicated overall representation.

--- src/Parser/HttpdConfigParser.hs
+++ src/Parser/HttpdConfigParser.hs
@@ -1,26 +1,16 @@
 
 module Parser.HttpdConfigParser where
 
-import Text.Parsec hiding (Line)
+import Text.Parsec
 import Text.Parsec.String
 import Control.Applicative hiding ((<|>), many)
-import Control.Monad
-
 -------------------------------------------------------------------------------
 -- Types
 -------------------------------------------------------------------------------
-newtype Config = Config [AbsDirective] deriving (Eq, Show)
 
-emptyConfig :: Config
-emptyConfig = Config ([])
+newtype Config = Config [Directive] deriving (Eq, Show)
 
-
-data AbsDirective   = D Directive | S SDirective deriving (Eq, Show)
-
-data Directive      = Directive { name :: String, args :: [String] }  deriving (Eq, Show)
-
-data SDirective     = SDirective Directive Config String deriving (Eq, Show)
-                  
+data Directive = Directive { name :: String, args :: [String], nds :: [Directive] } | EmptyDirective deriving (Eq, Show)
 
 -------------------------------------------------------------------------------
 -- Parsers
@@ -28,101 +18,106 @@ data SDirective     = SDirective Directive Config String deriving (Eq, Show)
 
 {-
     configp : parser for apache configuration
-    
-    This is the top-level function for the overall config parser. 
+
+    This is the top-level function for the overall config parser.
 -}
 configp :: Parser Config
 configp = Config <$> many1 directivep
 
-    
+
 {-
     directivep : high level parser for directives
 -}
-directivep :: Parser AbsDirective
+directivep :: Parser Directive
 directivep = skipMany whitespace
-    *> skipMany commentp 
-    *> ( try ( S <$> sectionDirectivep )
-        <|> ( D <$> simpleDirectivep ) <?> "Directive" ) 
-    <* skipMany commentp 
+    *> skipMany commentp
+    *> ( try (  sectionDirectivep )
+        <|> ( simpleDirectivep ) <?> "Directive" )
+    <* skipMany commentp
     <* skipMany whitespace
-    
-    
+
+
 {-
     sectionDirectivep : parser for section directives
--}    
-sectionDirectivep :: Parser SDirective
-sectionDirectivep = SDirective
-    <$> sectionOpenp   
-    <*> ( try ( lookAhead ( sectionClosep >> return emptyConfig ) )
-                    <|> configp <?> "SectionClose or Config" )
+-}
+sectionDirectivep :: Parser Directive
+sectionDirectivep = sectionDirective
+    <$> sectionOpenp
+    <*> ( try ( lookAhead ( sectionClosep >> return [] ) )
+                    <|> many1 directivep <?> "SectionClose or Config" )
     <*> sectionClosep
-    
-    
+    where   sectionDirective :: Directive -> [Directive] -> String -> Directive
+            sectionDirective (Directive n as _) nds _ = Directive n as nds
+
+
 {-
     simpleDirectivep : parser for simple directive
-    
-    A SimpleDirective must have a DirectiveName and can have 0 or more 
+
+    A SimpleDirective must have a DirectiveName and can have 0 or more
     arguments. the DirectiveName must be seperated from its arguments
-    by one or more spaces and the arguments, if more than one, must 
+    by one or more spaces and the arguments, if more than one, must
     also be seperated from each other by one or more spaces.
 -}
 simpleDirectivep :: Parser Directive
-simpleDirectivep = Directive 
-    <$> directiveNamep 
-    <*  ( many $ oneOf " " ) 
+simpleDirectivep = simpleDirective
+    <$> directiveNamep
+    <*  ( many $ oneOf " " )
     <*> ( endBy directiveArgp $ many $ oneOf " " )
+    where   simpleDirective :: String -> [String] -> Directive
+            simpleDirective n xs = Directive n xs []
 
-   
 {-
     sectionOpenp : parser for opening sections of section directives
--}    
+-}
 sectionOpenp :: Parser Directive
-sectionOpenp = char '<' *> simpleDirectivep <* char '>' <*  skipMany newline 
-    <?> "SectionOpen"    
+sectionOpenp = char '<' *> simpleDirectivep <* char '>' <*  skipMany newline
+    <?> "SectionOpen"
 
-    
+
 {-
     sectionClosep : parser for closing sections of section directives
 -}
 sectionClosep :: Parser String
-sectionClosep = ( char '<' *> char '/' *> directiveNamep <* char '>' )  
-    <* skipMany whitespace 
+sectionClosep = ( char '<' *> char '/' *> directiveNamep <* char '>' )
+    <* skipMany whitespace
     <?> "SectionClose"
-    
-    
+
+
 {-
     directiveNamep : parser for directive names
 -}
 directiveNamep :: Parser String
-directiveNamep = (:) <$> letter <*> many alphaNum    
-    
-    
+directiveNamep = (:) <$> letter <*> many alphaNum
+
+
 {-
     directiveArgp : parser for directive arguments
 -}
 directiveArgp :: Parser String
-directiveArgp = many1 dArgAllowed <?> "DirectiveArg"    
-    
+directiveArgp = many1 dArgAllowed <?> "DirectiveArg"
+
 
 {-
     dArgAllowed : the allowed characters in a directive argument
 -}
 dArgAllowed :: Parser Char
-dArgAllowed = try ( alphaNum ) <|> oneOf "/~.-_,\"\\^" 
-    
+dArgAllowed = try ( alphaNum ) <|> oneOf "/~.-_,\"\\^"
+
 
 {-
     commentp : parser for comments
 -}
 commentp :: Parser ()
-commentp = char '#' 
-    *> try ( skipMany ( noneOf "\n\r" ) ) 
+commentp = char '#'
+    *> try ( skipMany ( noneOf "\n\r" ) )
     *> skipMany whitespace
-    
-       
+
+
 {-
     whitespace : parser for whitespace
 -}
 whitespace :: Parser ()
 whitespace = space >> spaces
+
+
 
