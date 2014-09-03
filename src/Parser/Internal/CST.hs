6a05715c9170aca80f9cabd74501bf84d13ff75b
Stephen O'Brien <wayofthepie@gmail.com>
2014-09-03 20:33:27 +0100

    Update comments and error messages

--- src/Parser/Internal/CST.hs
+++ src/Parser/Internal/CST.hs
@@ -34,135 +34,118 @@ nested = _nds
 -- Parsers
 -------------------------------------------------------------------------------
 
-{-
-    configp : parser for apache configuration
 
-    This is the top-level function for the overall config parser.
--}
+-- | configp : parser for apache configuration
+-- 
+-- The top-level function for the overall config parser.
 configp :: Parser CST
 configp = CST <$> manyTill directivep eof
 
 
-{-
-    directivep : high level parser for directives
--}
+-- | directivep : high level parser for directives
 directivep :: Parser Directive
 directivep = skipMany ( commentp <|> whitespace)
     *> ( try ( sectionDirectivep ) <|> simpleDirectivep )
     <*  newline <* skipMany ( commentp <|> whitespace ) 
-    <?> "Expecting a directive"
+    <?> "a directive"
 
 
-{-
-    sectionDirectivep : parser for section directives
--}
+-- | sectionDirectivep : parser for section directives
 sectionDirectivep :: Parser Directive
 sectionDirectivep = sectionDirective
     <$> sectionOpenp
     <*  skipMany ( commentp <|> whitespace )
     <*> ( try ( lookAhead ( sectionClosep >> return [] ) ) <|> many1 directivep )
     <*  sectionClosep
-    <?> "Expecting a section directive"
+    <?> "a section directive"
     where   sectionDirective :: Directive -> [Directive] ->  Directive
             sectionDirective (Directive n as _) nds = Directive n as nds
 
 
-
-{-
-    simpleDirectivep : parser for simple directive
-
-    A SimpleDirective must have a DirectiveName and can have 0 or more
-    arguments. the DirectiveName must be seperated from its arguments
-    by one or more spaces and the arguments, if more than one, must
-    also be seperated from each other by one or more spaces.
--}
+-- | simpleDirectivep : parser for simple directive
+-- 
+-- A SimpleDirective must have a DirectiveName and can have 0 or more
+-- arguments. the DirectiveName must be seperated from its arguments
+-- by one or more spaces and the arguments, if more than one, must
+-- also be seperated from each other by one or more spaces.
 simpleDirectivep :: Parser Directive
 simpleDirectivep = simpleDirective
     <$> directiveNamep
     <*  ( many $ char ' ' )
     <*> ( sepBy ( try ( qdirectiveArgp ) <|> directiveArgp  ) $ oneOf " " )
-    <?> "Expecting a simple directive"
+    <?> "a simple directive"
     where   simpleDirective :: String -> [String] -> Directive
             simpleDirective n xs = Directive n xs []
 
 
-{-
-    sectionOpenp : parser for opening sections of section directives
--}
+-- | sectionOpenp : parser for opening sections of section directives
 sectionOpenp :: Parser Directive
 sectionOpenp = char '<' *> simpleDirectivep <* char '>' 
-    <?> "Expecting the opening section of a section directive"
+    <?> "the opening section of a section directive"
 
 
-{-
-    sectionClosep : parser for closing sections of section directives
--}
+-- | sectionClosep : parser for closing sections of section directives
 sectionClosep :: Parser String
 sectionClosep = ( char '<' *> char '/' *> directiveNamep <* char '>' )
-    <?> "Expecting the closing section of a section directive"
+    <?> "the closing section of a section directive"
 
 
-{-
-    directiveNamep : parser for directive names
--}
+-- | directiveNamep : parser for directive names
 directiveNamep :: Parser String
 directiveNamep = (:) <$> letter <*> many alphaNum 
-    <?> "Expecting a directive name"
+    <?> "a directive name"
 
 
 -- | qdirectiveArgp : parser for quoted directive arguments
 qdirectiveArgp :: Parser String
 qdirectiveArgp = between ( char '\"' ) ( char '\"' ) allowedChars
-    <?> "Expected a quoted directive argument"
+    <?> "a quoted directive argument"
     where allowedChars = many ( qdArgAllowed <|> escapedp <|> oneOf " " )
 
 
 -- |  escapedp : parser for escaped characters
 escapedp :: Parser Char
 escapedp = char '\\' >> choice charMap 
-    <?> "Expecting an escaped character"
+    <?> "an escaped character"
     where charMap = foldl (\l (c,r) -> (char c *> return r) : l)
                         [] escapedCharMappings
 
 
--- | transformChar : discards 'c' and returns a parser for its replacement
--- character 'replacement'
+-- | transformChar : discards 'c' and returns a parser for its replacement char 'r'
 transformChar :: Char -> Char -> Parser Char
-transformChar c replacement = char c *> return replacement
+transformChar c r = char c *> return r
 
 
 -- | escapedCharMappings : escaped characters and their replacements
 --
--- The first char in a tuple is the escaped character, teh second is what it
+-- The first char in a tuple is the escaped character, the second is what it
 -- must be replaced with when returning its value. Note that when parsing
 -- escaped characters are those preceeded with '\' e.g. "\n".
 escapedCharMappings :: [ ( Char, Char ) ]
 escapedCharMappings = [('\\', '\\'), ('\"', '\"'), ('n', '\n'), ('t', '\t')]
 
 
-{-
-    directiveArgp : parser for directive arguments
-
-    This parser parsers Directive arguments quoted and unquoted. For the former
-    the parsed string will retain its quotes, escaped.
--}
+-- | directiveArgp : parser for directive arguments
+-- 
+-- This parser parsers Directive arguments quoted and unquoted. For the former
+-- the parsed string will retain its quotes, escaped.
 directiveArgp :: Parser String
-directiveArgp = many dArgAllowed  <?> "Expecting a directive argument"
+directiveArgp = many dArgAllowed  <?> "a directive argument"
 
 
-{-
-    dArgAllowed : the allowed characters in a directive argument
--}
+-- | dArgAllowed : parser for the allowed characters in a directive argument
 dArgAllowed :: Parser Char
-dArgAllowed = alphaNum <|> oneOf dArgAllowedSymbols
+dArgAllowed = alphaNum <|> oneOf dArgAllowedSymbols 
+    <?> "a symbol for a directive argument"
 
 
+-- | qdArgAllowed : parser for the allowed characters in a quoted directive argument
 qdArgAllowed :: Parser Char
 qdArgAllowed = dArgAllowed <|> oneOf qdArgAllowedSymbols 
+    <?> "a symbol for a quoted directive argument"
 
-{-
-    dArgAllowedSymbols : list of allowed unescaped characters
--}
+
+-- | dArgAllowedSymbols : list of allowed unescaped characters
 dArgAllowedSymbols :: [Char]
 dArgAllowedSymbols = [ '/', '~', '@', '.', '-', '_', 
     ',', '^', ':', '*', '%', '{', '}' ]
@@ -173,18 +156,13 @@ qdArgAllowedSymbols :: [Char]
 qdArgAllowedSymbols = ['<', '>']
 
 
-{-
-    commentp : parser for comments
--}
+-- | commentp : parser for comments
 commentp :: Parser ()
 commentp =  char '#' *> manyTill anyChar newline  *> return ()
+    <?> "a comment"
 
 
-{-
-    whitespace : parser for whitespace
--}
+-- | whitespace : parser for whitespace
 whitespace :: Parser ()
 whitespace = space >> spaces
-
-
 
